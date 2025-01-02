7f950a07774735f19be59a7d03d67c2015a1876f
Nur Waqid M <waqid.id@gmail.com>
2025-01-02 09:32:16 +0700

    Update helloworld.php

--- examples/helloworld.php
+++ examples/helloworld.php
@@ -1,14 +1,21 @@
 <?php
 
-$stdin = fopen("php://stdin","r");
+// Open standard input for reading
+$stdin = fopen("php://stdin", "r");
 
+// Print a welcome message
 print "This is a two-way Hello World application.\n";
 print "Please enter your name: ";
-$name = trim(fgets($stdin));
-print "So,
 
-	World: Hello $name!!
-	$name: Hello World!!
-	
-Now you're friends.
-";
+// Read the user's input and trim any whitespace
+$name = trim(fgets($stdin));
+
+// Print a friendly greeting
+print "So,\n\n";
+print "\tWorld: Hello $name!!\n";
+print "\t$name: Hello World!!\n\n";
+print "Now you're friends.\n";
+
+// Close the standard input
+fclose($stdin);
+?>
